b86f0bcf0cefa391560c70a409c56c8e553565d1
jenaarmaan <armaan.jena08@gmail.com>
2026-06-20 12:53:04 +0530

    Added .python-version file specifying 3.12 runtime

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add backend directory to the Python path to import app correctly
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "backend"))
-
-# Import main FastAPI application instance
-from app.main import app
